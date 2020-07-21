20e71772608d2cf83adfbea2fefe35fad53630f5
modymor8 <50548442+modymor8@users.noreply.github.com>
2020-07-21 18:04:26 +0200

    config.php

--- config.php
+++ config.php
@@ -2,6 +2,6 @@
 
 $eksternal_version = "xxx";
 $version = "xxx";
-$token = "xxx";
-$uuid = "xxx";
-$userid = "xxx";
+$token = "Yjk0Y2Y0NjQtYzVlZC00YmZjLTg1YjAtZTAzMzkzYjg2ZmZj";
+$uuid = "f53c4415-8dae-4761-94a3-fa46265bced3";
+$userid = "16235149";
